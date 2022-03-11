a10466e9d714f291cb0a36fa2a7d3320138eb5db
sunyfg <yanfeng_sun@yeah.net>
2022-03-12 02:13:09 +0800

    add setup ref

---
+++ 02_src_setup/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>姓名：{{name}}</h1>
+  <h2>年龄：{{age}}</h2>
+  <button @click="say">说话</button>
+</template>
+
+<script>
+import { h } from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    let name = '张三'
+    let age = 20
+
+    function say() {
+      console.log(`你好，我叫${name}，我${age}岁了。`)
+    }
+
+    // 返回一个对象
+    return {
+      name,
+      age,
+      say
+    }
+
+    // 返回一个渲染函数
+    /* return () => h('h1', `你好，我叫${name}，我${age}岁了。`) */
+  }
+}
+</script>
