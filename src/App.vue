e35495398b4d15512f19400be489a28e8d23d652
sunyfg <yanfeng_sun@yeah.net>
2022-03-14 20:28:13 +0800

    响应式原理

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,33 @@
 <template>
-  <h1>姓名：{{ person.name }}</h1>
+  <h1>Vue2 响应式</h1>
+  <h2 v-show="person.name">姓名：{{ person.name }}</h2>
   <h2>年龄：{{ person.age }}</h2>
-  <h2>工作：{{ person.job.type }}</h2>
-  <h2>薪水：{{ person.job.salary }}K</h2>
-  <h2>爱好：{{ person.hobby }}</h2>
-  <h2>测试数据：{{ person.job.a.b.c }}</h2>
-  <button @click="changeInfo">修改信息</button>
+  <h2 v-show="person.sex">性别：{{ person.sex }}</h2>
+  <button @click="addSex">添加一个 sex 属性</button>
+  <button @click="deleteName">删除 name 属性</button>
 </template>
 
 <script>
 import { ref, reactive } from 'vue'
 export default {
   name: 'App',
-  setup() {
-    let person = reactive({
-      name: '张三',
-      age: 20,
-      job: {
-        type: '前端',
-        salary: 30,
-        a: {
-          b: {
-            c: 111,
-          },
-        },
-      },
-      hobby: ['音乐', '电影', '登山'],
-    })
-
-    function changeInfo() {
-      console.log(person)
-    }
-
-    // 返回一个对象
+  data() {
     return {
-      person,
-      changeInfo,
+      person: {
+        name: '张三',
+        age: 18,
+      },
     }
+  },
+  methods: {
+    addSex() {
+      this.person.sex = '男'
+      console.log(this.person)
+    },
+    deleteName() {
+      delete this.person.name
+      console.log(this.person)
+    },
   },
 }
 </script>
